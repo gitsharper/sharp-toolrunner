a63ecc533f28e51a12daa6bccaac83c382851d5e
joe mclain <jmclain@outlook.com>
2016-11-25 20:20:44 -0800

    Copy local and vxmainfest

--- CopyLocal.bat
+++ CopyLocal.bat
@@ -1,7 +1,7 @@
 ﻿@echo off
 cd ".\Custom Tool\bin\debug"
 cd
-copy STRCustomTool.dll;Sharp-ToolRunner.dll;STRunner.exe;tool-runner.cfg.json "C:\Users\joemc\AppData\Local\Microsoft\VisualStudio\14.0\Extensions\vsuuds13.doy"
-copy STRCustomTool.dll;Sharp-ToolRunner.dll;STRunner.exe;tool-runner.cfg.json "C:\Program Files (x86)\Nmp"
+copy STRCustomTool.dll;Sharp-ToolRunner.dll;Newtonsoft.Json.dll;STRunner.exe;tool-runner.cfg.json "C:\Users\joemc\AppData\Local\Microsoft\VisualStudio\15.0_c7aa7ec9\Extensions\bpzne5yt.pwi"
+copy STRCustomTool.dll;Sharp-ToolRunner.dll;Newtonsoft.Json.dll;STRunner.exe;tool-runner.cfg.json "C:\Program Files (x86)\Sharp-ToolRunner"
 
 
